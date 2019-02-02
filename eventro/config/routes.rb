3baaaa753c01fc31d1d3383960dec84c6425871c
nalghamdi <nalghamdi_@hotmail.com>
2019-02-02 17:48:07 +0300

    Auth

--- eventro/config/routes.rb
+++ eventro/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :sessions, only: [:create , :destroy]
+  
+  post "/users/register", to: "users#create"
+  get "/users/:id", to: "users#show"
+  get "/users", to: "users#index"
 end
