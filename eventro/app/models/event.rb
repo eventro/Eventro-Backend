01fbabfceef736203aba11085d223ec9519f038d
SultanBS <alsubaie.sultan.b@gmail.com>
2019-02-04 10:07:03 +0300

    save place

--- eventro/app/models/event.rb
+++ eventro/app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :likes, dependent: :delete_all
   has_many :attendees, dependent: :delete_all
+  # has_many :user, through: :comments, :likes, :attendees
+
 end
