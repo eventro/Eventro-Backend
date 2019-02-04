6c049f4f146aaab5937397a7a45ffe1c79ddbeb4
SultanBS <alsubaie.sultan.b@gmail.com>
2019-02-04 15:49:53 +0300

    Follow and likes controller finished

--- eventro/app/controllers/likes_controller.rb
+++ eventro/app/controllers/likes_controller.rb
@@ -1,2 +1,31 @@
 class LikesController < ApplicationController
+    before_action :require_token
+    before_action :set_event, only: [:count]
+
+    def create
+       @like= @current_user.Like.Create(set_params)
+        render Json: @like
+    end
+
+    def destroy
+        # @like = Like.find_by(set_params)
+        if  @like.user_id == @current_user.id
+        @like.destroy
+        render Json: {message: "Success"}
+
+    end
+    
+    def count
+        @like = @event.likes.count
+    end
+
+    private
+
+    def set_params
+        {
+            event_id: params.require(:event_id)
+            user_id: params.require(:user_id)
+        }
+    end
+    
 end
--- eventro/app/models/event.rb
+++ eventro/app/models/event.rb
@@ -4,5 +4,7 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :likes, dependent: :delete_all
   has_many :attendees, dependent: :delete_all
-  has_many :users, through: :comments
+  has_many :users, through: [:comments, :attendees]
+  
+
 end
