1422b00c4e609a5f8dd95a6562f8ea3f55561b24
arfajraa <arfajraa@gmail.com>
2019-02-05 12:52:59 +0300

    attendees, likes controllers, event model, routes

--- eventro/app/controllers/likes_controller.rb
+++ eventro/app/controllers/likes_controller.rb
@@ -1,32 +1,39 @@
 class LikesController < ApplicationController
-    before_action :require_token
-    before_action :set_event, only: [:count]
+  before_action :require_token
+  before_action :set_event, only: :count
+  before_action :set_like, only: :destroy
 
-    def create
-       @like= @current_user.Like.Create(set_params)
-        render Json: @like
-    end
+  def create
+    @like = @current_user.likes.create!(like_params)
+    render json: @like
+  end
 
-    def destroy
-        # @like = Like.find_by(set_params)
-        if  @like.user_id == @current_user.id
+  def destroy
+    if @like
+      if @like.user_id == @current_user.id
         @like.destroy
-        render Json: {message: "Success"}
+        render json: {message: "Deleted like successfully"}
+      end
+    else
+      render json: {message: "Can't destroy, already not liked"}
     end
+  end
 
-    end
-    
-    def count
-        @like = @event.likes.count
-    end
+  def count
+    @likecount = @event.likes.count
+    render json: @likecount
+  end
 
-    private
+  private
 
-    def set_params
-        {
-            event_id: params.require(:event_id)
-            user_id: params.require(:user_id)
-        }
-    end
-    
+  def set_like
+    @event = Event.find(params[:event_id])
+    @like = Like.find_by({user_id: @current_user.id, event_id: @event.id})
+  end
+
+  def like_params
+    {
+      event_id: params[:event_id],
+    }
+  end
 end
--- eventro/app/models/attendee.rb
+++ eventro/app/models/attendee.rb
@@ -1,4 +1,6 @@
 class Attendee < ApplicationRecord
+  validates :user_id, uniqueness: {scope: :event,
+                                   message: "Already attending"}
   belongs_to :user
   belongs_to :event
 end
