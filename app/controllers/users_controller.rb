8c7eebd8d8f6e404bc6b9be2acb3c81156b26b7c
nalghamdi <nalghamdi_@hotmail.com>
2019-02-06 13:32:18 +0300

    heroku deployment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,39 @@
 class UsersController < ApplicationController
   # NOT SURE IF WE NEED THE SKIP
-#   skip_before_action :verify_authenticity_token, only: [:create] 
-before_action :require_token, only: [:update]
-before_action :set_user, only: [:update, :show]
+  # skip_before_action :verify_authenticity_token, only: [:create] 
+  before_action :require_token, only: [:update]
+  before_action :set_user, only: [:update, :show]
 
-def index 
-  @users = User.all 
-  render json: @users
-end
+  def index 
+    @users = User.all 
+    render json: @users
+  end
 
-def show
+  def show
     render json: @user
   end
 
 
-def create
-  @user = User.create!(user_params)
-  render json: @user
-end
-
-def update 
-  if params[:id] == @current_user.id.to_s
-  @user.update_attributes(user_params)
-  render json: @user
+  def create
+    @user = User.create!(user_params)
+    render json: @user
   end
-end 
 
-private
+  def update 
+    if params[:id] == @current_user.id.to_s
+      @user.update_attributes(user_params)
+      render json: @user
+    end
+  end 
 
-def user_params
-  params.require(:user).permit(:username, :email, :password , :date_of_birth , :name)
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :date_of_birth, :name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
-
-def set_user
-  @user = User.find(params[:id])
-end
-
 
